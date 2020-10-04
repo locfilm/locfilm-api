11dc97d78b22baf731bf8ead850580f6b86d48bb
Angel Flores <cyberangel1000@gmail.com>
2020-10-03 20:35:17 -0500

    [FIX] Image field in location model and serializer

--- locfilm/locations/models/locations.py
+++ locfilm/locations/models/locations.py
@@ -23,6 +23,8 @@ class Location (models.Model):
         message="Phone number must be entered in the format : +999999999. Up to 15 digits allowed."
     )
     contact_phone = models.CharField(validators=[phone_regex],max_length=16)
+    main_image = models.ImageField(upload_to='locations/pictures', verbose_name='Main picture of a location', null=True)
+
 
     # Foreign Keys
 
