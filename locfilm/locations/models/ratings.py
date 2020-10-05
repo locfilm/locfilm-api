8a3e63bf23ef6d8d00f1383bd9d461cfbb0316cd
Angel Flores <cyberangel1000@gmail.com>
2020-10-05 00:44:27 -0500

    [FIX] Modified ratings table

--- locfilm/locations/models/ratings.py
+++ locfilm/locations/models/ratings.py
@@ -6,13 +6,15 @@ class Rating(models.Model):
        A ranting consist in a evaluation of level of satisfatiction of the customer about the
        quality of the booking
     """
-    RATINGS_CHOICES = ['1','2','3','4','5']
+    # RATINGS_CHOICES = ['1','2','3','4','5']
 
     location_id = models.ForeignKey('locations.Location', on_delete=models.CASCADE)
     booking_id = models.ForeignKey('bookings.Booking', on_delete=models.CASCADE)
     rating_date = models.DateTimeField(verbose_name='Rating date')
-    accesibility = models.CharField(null=False, verbose_name='Accesibility of location', choices= RATINGS_CHOICES, default=1),
-    conditions = models.CharField(null=False, verbose_name='Conditions of location', choices= RATINGS_CHOICES, default=1),
-    average = models.CharField(null=False, verbose_name='value for money of the location', choices= RATINGS_CHOICES, default=1),
+
+    # Ratings
+    accesibility = models.SmallIntegerField(null=False, verbose_name='Accesibility of location', default=0)
+    conditions = models.SmallIntegerField(null=False, verbose_name='Conditions of location', default=0)
+    average = models.SmallIntegerField(null=False, verbose_name='value for money of the location', default=0)
     description = models.CharField(null=True, max_length=2000,verbose_name='Observations about the experience of the user')
 
