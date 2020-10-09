6f60359d3ae3a78403e4e4f907956ee820aad193
Angel Flores <cyberangel1000@gmail.com>
2020-10-09 15:42:30 -0500

    [FEATURE] Script to add fake users to DB

---
+++ locfilm/users/test/create_fake_users.py
@@ -0,0 +1,37 @@
+# Create new objects in db
+
+from locfilm.users.models import User
+from django.forms.models import model_to_dict
+
+from .factories import UserFactory
+from locfilm.users.serializers import CreateUserSerializer
+
+for i in range(100):
+    # Crear un nuevo usuario del faker
+    new_user = model_to_dict(UserFactory.build())
+
+    # Serializer
+    serializer = CreateUserSerializer(data=self.user_data)
+
+    # Agregar usuario a BD con Serializer
+    serializer.is_valid()
+    serializer.create()
+
+
+# # Crear locaciones
+# for i in nuevo usuario del faker
+#     new_location = n range(100):
+#     # Crear umodel_to_dict(UserFactory.build())
+#     new_location_id = new_location.id
+
+#     # Crear images
+#     for i in range(10):
+#         new_image = image_fake.build(location_id=new_location_id)
+
+
+#     # Serializer
+#     serializer = CreateUserSerializer(data=self.user_data)
+
+#     # Agregar usuario a BD con Serializer
+#     serializer.is_valid()
+#     serializer.create()
