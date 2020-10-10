fb4dfc9a67c800a83e5911729dc8378a4f6ca539
Angel Flores Ayala <cyberangel1000@gmail.com>
2020-10-10 17:51:48 -0500

    [FIX] Image field in user serializer

--- locfilm/users/serializers.py
+++ locfilm/users/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'address')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'address', 'picture')
         read_only_fields = ('username', 'email')
 
 
